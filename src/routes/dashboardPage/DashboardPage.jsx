a8aefa006ccce2cca5be1bfa97cd6096338e5f87
Abdul Hafeez <hafeezrahimoon531@gmail.com>
2024-07-29 18:31:31 +0500

    complete the dashboard section design

--- src/routes/dashboardPage/DashboardPage.jsx
+++ src/routes/dashboardPage/DashboardPage.jsx
@@ -1,6 +1,37 @@
 import "./dashboardPage.css";
 const DashboardPage = () => {
-  return <div>DashboardPage</div>;
+  return (
+    <div className="dashboardPage">
+      <div className="texts">
+        <div className="logo">
+          <img src="/logo.png" alt="logo" />
+          <h1>Hz AI</h1>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src="/chat.png" alt="chat" />
+            <span>Create a New Chat</span>
+          </div>
+          <div className="option">
+            <img src="/image.png" alt="chat" />
+            <span>Analyze Images</span>
+          </div>
+          <div className="option">
+            <img src="/code.png" alt="chat" />
+            <span>Help me with my code</span>
+          </div>
+        </div>
+      </div>
+      <div className="formContainer">
+        <form>
+          <input type="text" placeholder="Ask me anything..." />
+          <button>
+            <img src="/arrow.png" alt="submit" />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
