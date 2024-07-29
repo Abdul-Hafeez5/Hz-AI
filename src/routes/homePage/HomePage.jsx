9d068ded004f5d70fceefd8d9860411af570a266
Abdul Hafeez <hafeezrahimoon531@gmail.com>
2024-07-29 14:37:30 +0500

    added a little aniamtion in homepage

--- src/routes/homePage/HomePage.jsx
+++ src/routes/homePage/HomePage.jsx
@@ -1,6 +1,10 @@
 import { Link } from "react-router-dom";
 import "./homePage.css";
+import { TypeAnimation } from "react-type-animation";
+import { useState } from "react";
+
 const HomePage = () => {
+  const [typingStatus, setTypingStatus] = useState("Human1");
   return (
     <div className="homePage">
       <img src="/orbital.png" alt="image" className="orbital" />
@@ -19,7 +23,55 @@ const HomePage = () => {
           <div className="bgContainer">
             <div className="bg"></div>
           </div>
-            <img src="/bot.png" alt="bot" className="bot" />
+          <img src="/bot.png" alt="bot" className="bot" />
+          <div className="chat">
+            <img
+              src={
+                typingStatus === "Human1"
+                  ? "/human1.jpeg"
+                  : typingStatus === "Human2"
+                  ? "/human2.jpeg"
+                  : "/bot.png"
+              }
+              alt="animatedImg"
+            />
+            <TypeAnimation
+              sequence={[
+                "Human:We produce food for Mice",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot: We produce food for Hamsters",
+                2000,
+                () => {
+                  setTypingStatus("Human2");
+                },
+                "Human2: We produce food for Guinea Pigs",
+                2000,
+                () => {
+                  setTypingStatus("bot");
+                },
+                "Bot: We produce food for Chinchillas",
+                2000,
+                () => {
+                  setTypingStatus("Human1");
+                },
+              ]}
+              wrapper="span"
+              repeat={Infinity}
+              cursor={true}
+              omitDeletionAnimation={true}
+            />
+          </div>
+        </div>
+      </div>
+      <div className="terms">
+        <img src="/logo.png" alt="" />
+        <div className="links">
+          <Link to={"/"}> Terms of service</Link>
+          <span>|</span>
+          <Link to={"/"}> Privacy Policy</Link>
         </div>
       </div>
     </div>
