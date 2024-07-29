4fc309bcc7b2066b8322edc3c5fb9e38d7be6952
Abdul Hafeez <hafeezrahimoon531@gmail.com>
2024-07-29 19:27:23 +0500

    complete the chat page

--- src/routes/chatPage/ChatPage.jsx
+++ src/routes/chatPage/ChatPage.jsx
@@ -1,10 +1,44 @@
-import "./chatPage.css"
+import { useRef } from "react";
+import "./chatPage.css";
+import { useEffect } from "react";
 const ChatPage = () => {
-  return (
-    <div>
-      ChatPage
-    </div>
-  )
-}
+  const lastRef = useRef(null);
 
-export default ChatPage
+  useEffect(() => {
+    lastRef.current.scrollIntoView({ behaviour: "smooth" });
+  }, []);
+
+  return (
+    <div className="chatPage">
+      <div className="wrapper">
+        <div className="chat">
+          <div className="message">Test message from AI</div>
+          <div className="message user">
+            test message from user Lorem ipsum dolor sit, amet consectetur
+            adipisicing elit. Facilis ratione numquam cumque, eligendi totam
+            iure obcaecati aliquid quos natus, nobis officiis laborum? Ex, modi!
+            Ab obcaecati quas delectus distinctio qui numquam, consequatur eaque
+            ea?
+          </div>
+          <div className="message">Test message from AI</div>
+          <div className="message user"> test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user"> test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user"> test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user"> test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user"> test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user"> test message from user</div>
+          <div className="message">Test message from AI</div>
+          <div className="message user"> test message from user</div>
+          <div ref={lastRef} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
